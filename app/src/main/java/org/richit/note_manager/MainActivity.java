0cc07c6e90edbf08753aca7a0bff869df4fb0d18
github_username <mdrayhan9464@gmail.com>
2021-04-17 16:09:30 +0600

    floating action button

--- app/src/main/java/org/richit/note_manager/MainActivity.java
+++ app/src/main/java/org/richit/note_manager/MainActivity.java
@@ -3,6 +3,11 @@ package org.richit.note_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText( MainActivity.this, "Hello world", Toast.LENGTH_SHORT ).show();
+            }
+        });
+
     }
 }
